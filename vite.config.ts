c245a7f128976171328814f1ad57f84ddbe39a82
KamranRiyaz <141331812+KamranRiyaz@users.noreply.github.com>
2026-07-05 14:00:19 +0530

    update the yml file for CI/CD integration using github actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? 'https://kamranriyaz.github.io/Portfolio/' : '/',
+  base: mode === 'production' ? '/Portfolio/' : '/',
   server: {
     host: "::",
     port: 8080,
